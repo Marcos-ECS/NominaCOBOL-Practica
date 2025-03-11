134265bdb6eeed5077d6a2b8a1227dbe7fc652c0
Marcos Canul <emir97008@outlook.com>
2025-03-11 01:56:54 -0600

    Datos basicos e ingreso de datos basico por CLI
    Solo se agrego ACCEPTS y mejora en el DATA DIVISION

--- Nomina.cbl
+++ Nomina.cbl
@@ -4,6 +4,24 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 NOMBRE-EMPLEADO     PIC A(50).
-       01 HORAS-TRABAJADAS    PIC 9(3).
+       01 NOMBRE-EMPLEADO     PIC X(50).
+       01 HORAS-TRABAJADAS    PIC 9(3)V99.
+       01 SALARIO-POR-HORA    PIC 9(4)V99.
+       01 DEDUCCIONES         PIC 9(3)V99.
+       01 SALARIO-BRUTO       PIC 9(3)V99.
+       01 SALARIO-NETO        PIC 9(3)V99.
+
+       PROCEDURE DIVISION.
+       INGRESO-DE-DATOS-DE-EMPLEADO.
+           DISPLAY "Ingrese el nombre del empleado".
+           ACCEPT NOMBRE-EMPLEADO.
+           DISPLAY "Ingrese el salario/sueldo por hora".
+           ACCEPT SALARIO-POR-HORA.
+           DISPLAY"Ingrese las horas trabajadas por semana".
+           ACCEPT HORAS-TRABAJADAS.
+           
+
+       RESULTADOS.
+           DISPLAY NOMBRE-EMPLEADO.
+           STOP RUN.
        END PROGRAM Nomina.
