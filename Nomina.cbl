bea3da2ca5b8fe26b460d93e7fbbcdf57ea209ee
Marcos Canul <emir97008@outlook.com>
2025-03-15 00:14:14 -0600

    Reporte basico y calculo basico
    Se agrego un reporte basico al meter datos mediante CLI, y calculos basico ya preestablecidos

--- Nomina.cbl
+++ Nomina.cbl
@@ -17,7 +17,10 @@
            PERFORM VALIDAR-NOMBRE.
            PERFORM VALIDAR-SALARIO-POR-HORA.
            PERFORM VALIDAR-HORAS-TRABAJADAS.
-           PERFORM RESULTADOS.
+           PERFORM CALCULAR-SALARIO-BRUTO.
+           PERFORM CALCULAR-DEDUCCIONES.
+           PERFORM CALCULAR-SALARIO-NETO.
+           PERFORM GENERAR-REPORTE.
            STOP RUN.
 
        VALIDAR-NOMBRE.
@@ -29,11 +32,11 @@
                PERFORM VALIDAR-NOMBRE.
 
        VALIDAR-SALARIO-POR-HORA.
-           DISPLAY "Ingrese el salario por hora (ejemplo: 15.75):".
+           DISPLAY "Ingrese el salario por hora en USD (Ej: 15.75):".
            ACCEPT SALARIO-POR-HORA.
            IF SALARIO-POR-HORA <= 0
-               DISPLAY "Error: El salario por"
-               DISPLAY"hora debe ser mayor que cero."
+               DISPLAY "==========ERROR=========="
+               DISPLAY"El salario por hora debe ser mayor que 0 USD."
                PERFORM VALIDAR-SALARIO-POR-HORA.
 
        VALIDAR-HORAS-TRABAJADAS.
@@ -41,11 +44,27 @@
            DISPLAY"(ejemplo: 40.5):"
            ACCEPT HORAS-TRABAJADAS.
            IF HORAS-TRABAJADAS < 0 OR HORAS-TRABAJADAS > 168
-               DISPLAY "Error: Las horas trabajadas deben estar"
-               DISPLAY"entre 0 y 168."
+               DISPLAY "==========ERROR=========="
+               DISPLAY"Las horas trabajadas deben estar entre 0 y 168."
                PERFORM VALIDAR-HORAS-TRABAJADAS.
+       
+       CALCULAR-SALARIO-BRUTO.
+           COMPUTE SALARIO-BRUTO = HORAS-TRABAJADAS * SALARIO-POR-HORA.
 
-       RESULTADOS.
-           DISPLAY NOMBRE-EMPLEADO
+       CALCULAR-DEDUCCIONES.
+           COMPUTE DEDUCCIONES = SALARIO-BRUTO * 0.15. *>15% de impuestos.
+
+       CALCULAR-SALARIO-NETO.
+           COMPUTE SALARIO-NETO = SALARIO-BRUTO - DEDUCCIONES.
+
+       GENERAR-REPORTE.
+           DISPLAY "----------------------------------------".
+           DISPLAY "Nombre del empleado: ", NOMBRE-EMPLEADO.
+           DISPLAY "Horas trabajadas: ", HORAS-TRABAJADAS.
+           DISPLAY "Salario por hora: ", SALARIO-POR-HORA.
+           DISPLAY "Salario bruto: ", SALARIO-BRUTO.
+           DISPLAY "Deducciones: ", DEDUCCIONES.
+           DISPLAY "Salario neto: ", SALARIO-NETO.
+           DISPLAY "----------------------------------------".
            STOP RUN.
        END PROGRAM Nomina.
